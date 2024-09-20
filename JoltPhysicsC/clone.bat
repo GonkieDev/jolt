984fc24ffe9a75f7b0ee84c600c871ecd7373cfe
Gonkie <cryusdev@gmail.com>
2024-09-20 10:12:47 +0100

    JoltPhysicsC (from zig) & clone.bat

---
+++ JoltPhysicsC/clone.bat
@@ -0,0 +1,17 @@
+@echo off
+pushd %~dp0
+
+set jolt_c=https://github.com/zig-gamedev/zig-gamedev/blob/main/libs/zphysics/libs/JoltC/
+
+:: Delete all .cpp and .h files
+del /q "*.cpp"
+del /q "*.h"
+
+curl -O %jolt_c%/JoltPhysicsC.cpp
+curl -O %jolt_c%/JoltPhysicsC.h
+curl -O %jolt_c%/JoltPhysicsC_Extensions.cpp
+
+:: Unset stuff
+set jolt_c=
+
+popd
